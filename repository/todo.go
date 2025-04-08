fb597cfdf1bad80680fb472a0acc7e8c912d35bb
70ekanetugu <12kanetugu@gmail.com>
2025-04-08 14:48:41 +0000

    add testRepository

--- repository/todo.go
+++ repository/todo.go
@@ -11,6 +11,7 @@ import (
 )
 
 var Db *gorm.DB
+var todoRepository TodoRepository
 
 type Todo struct {
 	// json変換時にkey名は小文字・スネークケースにしたいのでgorm.Modelは使用しない。
@@ -22,8 +23,32 @@ type Todo struct {
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
 }
 
+type TodoRepository interface {
+	getTodos(startId, endId uint) ([]Todo, error)
+	getTodoById(id uint) (*Todo, error)
+	saveTodo(todo *Todo) error
+}
+
+type dbRepository struct{}
+type testRepository struct{}
+
 // Todo一覧を startId~endId の範囲で取得する。
-func GetTodos(startId, endId uint) (todos []Todo, err error) {
+func GetTodos(startId, endId uint) ([]Todo, error) {
+	return todoRepository.getTodos(startId, endId)
+}
+
+// 指定idのTodoを取得する。
+func GetTodoById(id uint) (*Todo, error) {
+	return todoRepository.getTodoById(id)
+}
+
+// IDがセットされていなければINSERT, セットされていればUPDATEする。
+func SaveTodo(todo *Todo) error {
+	return todoRepository.saveTodo(todo)
+}
+
+// dbRepositoryの実装
+func (d *dbRepository) getTodos(startId, endId uint) (todos []Todo, err error) {
 	todos = make([]Todo, endId-startId+1)
 	if err = Db.Where("id >= ? AND id <= ?", startId, endId).Find(&todos).Error; err == nil {
 		return todos, nil
@@ -35,8 +60,7 @@ func GetTodos(startId, endId uint) (todos []Todo, err error) {
 	}
 }
 
-// 指定idのTodoを取得する。
-func GetTodoById(id uint) (todo *Todo, err error) {
+func (d *dbRepository) getTodoById(id uint) (todo *Todo, err error) {
 	todo = new(Todo)
 	if err = Db.First(todo, id).Error; err == nil {
 		return todo, nil
@@ -48,13 +72,39 @@ func GetTodoById(id uint) (todo *Todo, err error) {
 	}
 }
 
-// IDがセットされていなければINSERT, セットされていればUPDATEする。
-func SaveTodo(todo *Todo) (err error) {
+func (d *dbRepository) saveTodo(todo *Todo) (err error) {
 	return Db.Save(todo).Error
+}
+
+// fsReepositoryの実装
+func (f *testRepository) getTodos(startId, endId uint) (todos []Todo, err error) {
+	todos = []Todo{
+		{ID: 1, Text: "Todo1", Completed: false, CreatedAt: time.Now(), UpdatedAt: time.Now()},
+		{ID: 2, Text: "Todo2", Completed: true, CreatedAt: time.Now(), UpdatedAt: time.Now()},
+		{ID: 3, Text: "Todo3", Completed: false, CreatedAt: time.Now(), UpdatedAt: time.Now()},
+		{ID: 4, Text: "Todo4", Completed: true, CreatedAt: time.Now(), UpdatedAt: time.Now()},
+		{ID: 5, Text: "Todo5", Completed: false, CreatedAt: time.Now(), UpdatedAt: time.Now()},
+	}
+	return todos, nil
+}
+
+func (f *testRepository) getTodoById(id uint) (todo *Todo, err error) {
+	todo = &Todo{ID: id, Text: "Todo" + fmt.Sprint(id), Completed: false, CreatedAt: time.Now(), UpdatedAt: time.Now()}
+	return todo, nil
+}
+
+func (f *testRepository) saveTodo(todo *Todo) (err error) {
+	return nil
 }
 
 // DBの初期化処理。サーバー起動前に呼び出すこと。
 func Initialize(dbHost, dbPort, dbUser, dbPass string) (err error) {
+	if dbHost == "" || dbPort == "" || dbUser == "" || dbPass == "" {
+		todoRepository = newTodoRepository(false)
+		slog.Info("DB connection info is not set. Use test repository.")
+		return nil
+	}
+
 	dsn := fmt.Sprintf(
 		"%s:%s@tcp(%s:%s)/todo_demo?charset=utf8mb4&parseTime=True&loc=Local",
 		dbUser,
@@ -73,5 +123,16 @@ func Initialize(dbHost, dbPort, dbUser, dbPass string) (err error) {
 		return err
 	}
 
+	slog.Info("DB connection established")
+	todoRepository = newTodoRepository(true)
 	return nil
+}
+
+// TodoRepositoryの実装を取得する。
+func newTodoRepository(useDb bool) TodoRepository {
+	if useDb {
+		return &dbRepository{}
+	} else {
+		return &testRepository{}
+	}
 }
